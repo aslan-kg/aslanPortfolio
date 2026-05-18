e1fa4d2606592e0504609caa37a795edb0d2ec21
Aslan Ortiz Gomez <aortiz164@west-mec.org>
2026-05-18 12:34:16 -0700

    Unique colors for the page

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -10,18 +10,20 @@ export const Home = () => {
           </h1>
 
           <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
-            Generic description about the education and career.
+            <li>Studied the West-Mec Coding course.</li>
+            <li>Certifiacation on ITS Java Script</li>
+            <li>Certification on HTML and CSS</li>
           </p>
           <div className="flex justify-center space-x-4">
             <a
               href="#projects"
-              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 140, 246, 0.4)]"
+              className="bg-red-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 140, 246, 0.4)]"
             >
               View Projects
             </a>
             <a
               href="#contact"
-              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200  hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 140, 246, 0.2)] hover:bg-blue-500/10"
+              className="border border-red-500/50 text-red-500 py-3 px-6 rounded font-medium transition-all duration-200  hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 140, 246, 0.2)] hover:bg-blue-500/10"
             >
               Contact Page
             </a>
---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,15 @@
+export const Projects = () => {
+    return (
+      <section
+        id="Projects"
+        className="min-h-screen flex items-center justify-center py-20 "
+      >
+        <div className="max-width-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-red-500 to-cyan-400  bg-clip-text text-transparent text-center">
+            Projects
+          </h2>
+        </div>
+      </section>
+    );
+
+}
